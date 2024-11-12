bc86075d5806e78fd153b00343e1bbbca25d2cfc
Windisley <windisleyl@gmail.com>
2024-11-12 00:20:19 -0300

    Adicionando fruta no array Fruits2 com Splice()

--- Splice/script.js
+++ Splice/script.js
@@ -1,13 +1,35 @@
+const container = document.querySelector(".container")
+
 const $ul = document.querySelector(".ul")
 console.log($ul.innerHTML)
 
-const fruits = ["Banana", "Uva", "Mexirica", "Melancia"]
-fruits.splice(0,1)
+const fruits1 = ["Banana", "Uva", "Mexirica", "Melancia"]
+fruits1.splice(0,1)
 
-fruits.map((fruit)=>{
+fruits1.map((fruit)=>{
     const $li = document.createElement("li")
     $li.innerHTML = fruit
     $ul.appendChild($li)
-
+    
     
 })
+
+const fruits2 = ["Banana", "Uva", "Mexirica", "Melancia"]
+fruits2.splice(0,1, "Laranja")
+
+const $ul2 = document.createElement("ul")
+container.appendChild($ul2)
+fruits2.map((fruit)=>{
+    
+    const $li = document.createElement("li")
+    const $p = document.createElement("p")
+
+    $li.style = "font-size: 1.5rem; font-family: sans-serif; letter-spacing: 1px; color: blue;"
+    $li.innerHTML = fruit
+    $ul2.appendChild($li)
+    
+
+    
+    
+})
+
